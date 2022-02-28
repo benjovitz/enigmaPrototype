e1d43986b65841d5c69bbfed74686f76dd41bd4c
Benjovitz <dbenjovitz@gmail.com>
2022-02-28 20:24:30 +0100

    https://github.com/benjovitz/enigmaPrototype.git

---
+++ src/com/company/Main.java
@@ -0,0 +1,60 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	char char1= ' ';
+    int int1;
+    int1 = char1;
+        //System.out.println(int1);
+
+       Main obj = new Main();
+        System.out.println(obj.letterToNumber(char1));
+        System.out.println(obj.letterToNumber2(char1));
+        int a = 65;
+        char c = (char)a;
+        System.out.println(a);
+        System.out.println(obj.numberToLetter(3));
+
+
+    }
+
+    public int letterToNumber(char letter) {
+
+        letter = Character.toUpperCase(letter);
+        int number=-1;
+        char[] alphabet = " ABCDEFGHIJKLMNOPQRSTUVXYZÆØÅ".toCharArray();
+        for (int i = 0; i < alphabet.length; i++) {
+        if(alphabet[i]==letter){
+            return i;
+        }
+        }
+
+        return number;
+    }
+    public int letterToNumber2(char letter){
+        //working but wonky IMO
+        //space= 32 Æ=198 Ø=216 Å=197
+        //ASCII values used in upper case A=65, for enigma A=1
+     letter = Character.toUpperCase(letter);
+        int number;
+        if(letter=='Æ'){
+            number =letter-171;
+        }else if (letter=='Ø'){
+            number=letter-188;
+        } else if (letter=='Å'){
+            number=letter-168;
+        } else if (letter==' '){
+            number= letter-32;
+        } else
+        number = letter-64;
+
+
+        return number;
+    }
+    public char numberToLetter(int number){
+        char[] alphabet = " ABCDEFGHIJKLMNOPQRSTUVXYZÆØÅ".toCharArray();
+        char letter = alphabet[number];
+        return letter;
+    }
+}
